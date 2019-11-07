be4b3556bbfb79bef3216b4489815fc9ba0872d7
Mitch O'Hair <mitchell.ohair@gmail.com>
2019-11-07 00:28:05 +0000

    Added add grade endpoint

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ const connection = require('./databaseConnection');
 const express = require('express');
 const server = express();
 
+server.use(express.urlencoded({extended: true}));
+server.use(express.json());
+
 server.get('/api/grades', (req, res) => {
   let query = "SELECT * FROM `grades`";
-  connection.query(query, (error, results, fields) => {
+  connection.query(query, (error, results) => {
     if (error) throw error;
     let response = results.map(row => {
                      return {
@@ -18,9 +21,20 @@ server.get('/api/grades', (req, res) => {
   });
 });
 
-// server.post('/api/grades', (req, res) => {
+server.post('/api/grades', (req, res) => {
+  let query = "INSERT INTO `grades` (`student_name`, `student_course`, `student_grade`) ";
+  query += `VALUES ('${req.body.name}', '${req.body.course}', ${req.body.grade})`;
+  connection.query(query, (error, result) => {
+    if ( error ) throw error;
+    res.send({
+      id: result.insertId,
+      name: req.body.name,
+      course: req.body.course,
+      grade: req.body.grade
+    });
+  });
 
-// });
+});
 
 // server.delete('/api/grades', (req, res) => {
 
